f511876031ca2f770d18619c96820e8515d85df6
pxdkxvan <alekseevamg2000@gmail.com>
2024-11-19 04:52:52 +0300

    validation and page change logic

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
+// import Swiper from 'swiper' // fix IDE warns
+
 const pages = new Swiper('.main__container', {
     direction: 'vertical',
+    speed: 600,
     slidesPerView: 1,
+    // effect: 'flip',
     spaceBetween: 0,
-    mousewheel: true,
-    simulateTouch: false,
-    allowTouchMove: true,
-    keyboard: { enabled: true },
+    mousewheel: false,
+    simulateTouch: false, // must be false always
+    allowTouchMove: false,
+    keyboard: false,
     touchStartPreventDefault: false,
 });
+
+// -------------------------------------------------------------------------------------------------------- //
+
+function enablePagesChange() {
+    pages.params.allowTouchMove = true;
+
+    pages.params.mousewheel.enabled = true;
+    pages.mousewheel.enable();
+
+    pages.params.keyboard.enabled = true;
+    pages.keyboard.enable();
+
+    pages.update();
+    pages.slideNext();
+}
 
 // -------------------------------------------------------------------------------------------------------- //
 
@@ -24,3 +43,31 @@ noUiSlider.create(timeBar, {
 
 // -------------------------------------------------------------------------------------------------------- //
 
+const genBtn = document.querySelector('.generate');
+genBtn.addEventListener('click', enablePagesChange, {once: true});
+
+// -------------------------------------------------------------------------------------------------------- //
+
+function validateTextField(event, nonNeg) {
+    const tf = event.target;
+
+    const value = tf.value;
+    const last = tf.selectionStart - 1;
+
+    const pattern = nonNeg ? /^\d*[.,]?\d*$/ : /^-?\d*[.,]?\d*$/;
+
+    if (!pattern.test(value)) tf.value = value.slice(0, last) + value.slice(last + 1);
+}
+
+const textInputList = document.querySelectorAll('.text-field');
+const checkboxInput = document.querySelector('.checkbox');
+
+textInputList.forEach(ti => {
+    ti.addEventListener('input', event => validateTextField(event, ti.classList.contains('non-neg')));
+    ti.addEventListener('paste', event => event.preventDefault());
+});
+
+// Возможно следует указать в каких значениях у нас варьируются данные
+// Также подумать над цветом залоченной кнопки
+
+// -------------------------------------------------------------------------------------------------------- //
